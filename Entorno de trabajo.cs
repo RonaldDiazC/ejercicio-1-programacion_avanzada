ee588ae3333d145e8b426022d1b3c362fde2d102
RonaldDiazC <128710997+RonaldDiazC@users.noreply.github.com>
2023-04-11 08:30:25 -0400

    Update and rename ejercicio 1 programacion_avanzada/producto.cs to Entorno de trabajo.cs

---
+++ Entorno de trabajo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entorno de trabajo 
+{
+    public class producto
+    {
+        //Tenemos repositorios para uso compartido dentro de Github para poder realizar modificaciones 
+        //dentro del codigo.
+
+        }
+    }
+}
+
